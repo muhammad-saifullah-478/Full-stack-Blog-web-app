80e1f26fb91315ccf4c48d61e5ee2b5ad1c838b9
Muhammad saifullah <saifkasana950@gmail.com>
2025-07-18 23:52:32 +0500

    web
    App

---
+++ module/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://saifkasana950:saif12345678@createprof.gp68tfb.mongodb.net/webali",
+  console.log("DB contect")
+);
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+  profilepic:{
+    type:String,
+    default:"sidbarpic.png"
+  },
+
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+});
+module.exports = mongoose.model("user", userSchema);
